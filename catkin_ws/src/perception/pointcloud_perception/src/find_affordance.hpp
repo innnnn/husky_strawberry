81cf32ffe7d3f46ed2d7bc3301857647832cc67a
sean85914 <sean19960914@gmail.com>
2019-03-27 14:50:57 +0800

    rename launch, pc perception done

--- catkin_ws/src/perception/pointcloud_perception/src/find_affordance.hpp
+++ catkin_ws/src/perception/pointcloud_perception/src/find_affordance.hpp
@@ -14,14 +14,19 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <geometry_msgs/Point.h>
 #include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseArray.h>
 #include <image_prediction/bboxList.h>
 #include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
 
 typedef pcl::PointCloud<pcl::PointXYZ> PointXYZ; // Not sure why xyzrgb cannot work
-typedef std::pair<int, int> Pixel;
-typedef std::pair<Pixel, int> PixelWithR; 
+typedef pcl::PointCloud<pcl::PointXYZRGB> PointXYZRGB;
+typedef std::pair<int, int> Pixel; // Image coordinate
+typedef std::pair<Pixel, int> PixelWithR; // Image coordinate with its corresponding R-ch value
 typedef std::vector<PixelWithR> PixelWithRVector;
 
+const double PCNUM_THRES = 100; // Threshold number of points to cossidered as target
+const double IMG_WIDTH = 640; // Image width
 const std::string FRAME = "camera_color_optical_frame";
 const std::string PC_STR = "/camera/depth_registered/points";
 
@@ -29,29 +34,59 @@ class PCPerception{
  private:
   ros::NodeHandle nh_, pnh_;
   ros::Subscriber sub_bbox;
-  ros::Publisher pub_marker, pub_pc;
+  ros::Publisher pub_marker, pub_pc, pub_aff;
   PointXYZ pc_;
+  bool verbose; // If true, also publish pc of last target
   double std_mul;
-  void initMarker(visualization_msgs::Marker&);
+  /* Initial marker with given id number
+     @param
+       visualization_msgs::Marker&: target marker
+       int: input id
+  */
+  void initMarker(visualization_msgs::Marker&, int);
+  /*
+      Callback function for subscriber
+      Extract the pixel in bounding box to point cloud and visualize the center
+  */
   void callback(const image_prediction::bboxList);
+  /*
+      Statistical processing for calculating mean and standard derivation to
+      find the inlier pixel
+      @param
+        const PixelWithRVector: given target array
+        double&: mean reference
+        double&: std reference
+  */
   void statistic_processing(const PixelWithRVector, double&, double&);
  public:
-  PCPerception(ros::NodeHandle nh, ros::NodeHandle pnh);
+  /*
+      Constructor
+      @param
+        ros::NodeHandle: public node handler
+        ros::NodeHandle: private one
+  */ 
+  PCPerception(ros::NodeHandle, ros::NodeHandle);
 };
 
 PCPerception::PCPerception(ros::NodeHandle nh, ros::NodeHandle pnh): \
 	nh_(nh), pnh_(pnh){
-  sub_bbox = nh_.subscribe("/bounding_box", 1, &PCPerception::callback, this);
-  pub_marker = pnh_.advertise<visualization_msgs::Marker>("affordance", 1);
+  sub_bbox = nh_.subscribe("bounding_box", 1, &PCPerception::callback, this);
+  pub_marker = pnh_.advertise<visualization_msgs::MarkerArray>("affordance_marker", 1);
+  pub_aff = pnh_.advertise<geometry_msgs::PoseArray>("affordance", 1);
   pub_pc = pnh_.advertise<sensor_msgs::PointCloud2>("points", 1);
   if(!pnh_.getParam("std_mul", std_mul)){
     std_mul = 0.5;
-    ROS_WARN("std_mul use default value");
+    ROS_WARN("std_mul use default value: %f", std_mul);
+  }
+  if(!pnh_.getParam("verbose", verbose)){
+    verbose = false;
+    ROS_WARN("std_mul use default value: %s", (verbose==true?"true":"false"));
   }
 }
 
-void PCPerception::initMarker(visualization_msgs::Marker& marker){
+void PCPerception::initMarker(visualization_msgs::Marker& marker, int id){
   marker.header.frame_id = FRAME;
+  marker.id = id;
   // Sphere
   marker.type = visualization_msgs::Marker::SPHERE;
   marker.action = visualization_msgs::Marker::ADD;
@@ -76,10 +111,12 @@ void PCPerception::callback(const image_prediction::bboxList msg){
    										 (PC_STR, ros::Duration(3.0));
   if(pc == NULL) {ROS_ERROR("Not point cloud received."); return;}
   pcl::fromROSMsg(*pc, pc_);
+  geometry_msgs::PoseArray poseArray;
+  visualization_msgs::MarkerArray markerArray;
   visualization_msgs::Marker marker;
-  initMarker(marker);
   PointXYZ temp;
   for(int i=0; i<numOfTarget; ++i){ 
+    initMarker(marker, i);
     PixelWithRVector pwrv;
     int range[4] = {msg.bbox[i].bb[0], msg.bbox[i].bb[1], msg.bbox[i].bb[2], msg.bbox[i].bb[3]};
     // Encode: xmin, ymin, xmax, ymax
@@ -94,43 +131,52 @@ void PCPerception::callback(const image_prediction::bboxList msg){
     statistic_processing(pwrv, mean, std);
     // Inlier
     PixelWithRVector inlier;
-    for(int num=0; num<pwrv.size(); ++num){
-      if(pwrv[num].second > std_mul*std+mean){
+    for(int num=0; num<pwrv.size(); ++num){ 
+      if(pwrv[num].second > std_mul*std+mean){ // Standardized data greater than std_mul 
         inlier.push_back(pwrv[num]);
       } // end if
     } // end num
-    // -------------*****************-------------*****************-------------*****************
     // Extract inlier pixels to pc
     for(int num=0; num<inlier.size(); ++num){
       //temp.push_back(pc_.at(inlier[num].first.first, inlier[num].first.second)); //Not Organized!!
-      int idx = inlier[num].first.second * 640 + inlier[num].first.first;
-      if(pc_.at(idx).x!=0. and pc_.at(idx).y!=0. and pc_.at(idx).z!=0.){
-        temp.push_back(pc_.at(idx));
-      } 
+      int idx = inlier[num].first.second * IMG_WIDTH + inlier[num].first.first;
+      if(idx>=pc_.size()){
+        ROS_WARN("Index out of range, ignore...");
+      } else temp.push_back(pc_.at(idx)); 
     } // end num
-    if(temp.size()<100){
+    if(temp.size()<PCNUM_THRES){
       ROS_WARN("Points too few, ignore...");
       return;
     }
-
     // Get centroid
     Eigen::Vector4f centroid; 
-    pcl::compute3DCentroid(temp,centroid); 
+    pcl::compute3DCentroid(temp,centroid);
+    // Check if nan
+    if(std::isnan(centroid[0])){
+      ROS_WARN("Get nan, ignore...");
+      return;
+    } 
     geometry_msgs::Pose pos; 
     pos.position.x = centroid[0]; pos.position.y = centroid[1]; pos.position.z = centroid[2];
     ROS_INFO("%f %f %f", centroid[0], centroid[1], centroid[2]);
     pos.orientation.w = 1.0;
     marker.pose = pos;
+    markerArray.markers.push_back(marker);
+    poseArray.poses.push_back(pos);
   } // end i
-  if(!std::isnan(marker.pose.position.x))
-    pub_marker.publish(marker);
-  else{ROS_WARN("Get nan. ignore...");}
-  sensor_msgs::PointCloud2 pcout;
-  pcl::PCLPointCloud2 pc2;
-  pcl::toPCLPointCloud2(temp, pc2); // First convert to pclpc2
-  pcl_conversions::fromPCL(pc2, pcout);
-  pcout.header.frame_id = FRAME;
-  pub_pc.publish(pcout); // Then publish
+  // Publish pose array
+  pub_aff.publish(poseArray);
+  // Publish marker
+  pub_marker.publish(markerArray);
+  // Publish pc
+  if(verbose){
+    sensor_msgs::PointCloud2 pcout;
+    pcl::PCLPointCloud2 pc2;
+    pcl::toPCLPointCloud2(temp, pc2); // First convert to pclpc2
+    pcl_conversions::fromPCL(pc2, pcout);
+    pcout.header.frame_id = FRAME; // Add frame id
+    pub_pc.publish(pcout); // Then publish
+  }
 }
 
 void PCPerception::statistic_processing(const PixelWithRVector pwrv, double& mean, double& std){
